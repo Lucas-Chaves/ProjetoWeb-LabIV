7cfc74b1ee2ca17c416bf11b2fa1303938894e09
Lucas Chaves <lucas.limachaves@outlook.com>
2020-07-06 17:37:23 -0300

    gitpod add

---
+++ DDL.sql
@@ -0,0 +1,33 @@
+
+
+create schema usuarios;
+use usuarios;
+
+CREATE USER 'lucas'@'localhost' IDENTIFIED BY 'lucas';
+GRANT ALL PRIVILEGES ON * . * TO 'lucas'@'localhost';
+
+FLUSH PRIVILEGES;
+
+CREATE TABLE `usuarios`.`user` (
+  `id_user` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(255) NOT NULL,
+  `user_email` VARCHAR(255) NOT NULL,
+  `user_pass` VARCHAR(255) NOT NULL,
+  `user_role` VARCHAR(255) NOT NULL,
+  `user_token` VARCHAR(255),
+  PRIMARY KEY (`id_user`));
+
+INSERT INTO`user`
+(
+`user_name`,
+`user_email`,
+`user_pass`,
+`user_role`,
+`user_token`)
+VALUES
+(
+"lucas",
+"lucas@gmail.com",
+"12345",
+"Admin",
+"sadasdasdasdasdasdasdasd");
---
+++ README.md
@@ -0,0 +1,4 @@
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/Lucas-Chaves/ProjetoWeb-LabIV)
+
+
+Para executar: mvn tomcat7:run
